e90a30035f0b3cd7096a0557ea06f72f3a44804c
Udit Padhan <uditpadhan98@gmail.com>
2023-06-30 06:22:29 +0530

    pagination changes

--- controllers/products.js
+++ controllers/products.js
@@ -29,17 +29,17 @@ const getAllProducts=async(req,res)=>{
         apiData=apiData.select(selectFix);
     }
 
-    let page=Number(req.query.page)||1;
-    let limit=Number(req.query.limit)||2;
+    // let page=Number(req.query.page)||1;
+    // let limit=Number(req.query.limit)||2;
 
-    let skip=(page-1)*limit;
+    // let skip=(page-1)*limit;
 
-    apiData=apiData.skip(skip).limit(limit);
+    // apiData=apiData.skip(skip).limit(limit);
 
     // console.log(queryObject);
 
     const myData=await apiData;
-    res.status(200).json({myData,bnHits:myData.length});
+    res.status(200).json({myData});
 };
 
 const getAllProductsTesting=async(req,res)=>{
